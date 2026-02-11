f2201dc0a433a08aab05dd447a48c5bcccef966c
vijayedstellar <vijay@edstellar.com>
2026-02-11 16:33:48 +0530

    noindex 3rd attempt

--- src/components/OrgChartTool.tsx
+++ src/components/OrgChartTool.tsx
@@ -9,7 +9,38 @@ function OrgChartTool() {
   const [currentPage, setCurrentPage] = useState<Page>('landing');
 
   return (
-    <div className="relative">
+    <div className="relative">import { useState } from 'react';
+    import LandingPage from './LandingPage';
+    import EmployeeImportLocal from './EmployeeImportLocal';
+    import OrgChartLocal from './OrgChartLocal';
+    
+    type Page = 'landing' | 'import' | 'chart';
+    
+    function OrgChartTool() {
+      const [currentPage, setCurrentPage] = useState<Page>('landing');
+    
+      return (
+        <div className="relative">
+          {currentPage === 'landing' && (
+            <LandingPage onNext={() => setCurrentPage('import')} />
+          )}
+    
+          {currentPage === 'import' && (
+            <EmployeeImportLocal
+              onNext={() => setCurrentPage('chart')}
+              onBack={() => setCurrentPage('landing')}
+            />
+          )}
+    
+          {currentPage === 'chart' && (
+            <OrgChartLocal onBack={() => setCurrentPage('import')} />
+          )}
+        </div>
+      );
+    }
+    
+    export default OrgChartTool;
+    
       {currentPage === 'landing' && (
         <LandingPage onNext={() => setCurrentPage('import')} />
       )}
